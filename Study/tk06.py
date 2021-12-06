0ec76bb7ce6f2f0f2fb2d53ce82091ddf10991be
GeoniNam <namgeoni0330@gmail.com>
2021-12-06 13:10:58 +0900

    예제 버튼 다시 티케07 쉼표주의

---
+++ Study/tk06.py
@@ -0,0 +1,11 @@
+from tkinter import *
+w = Tk()
+
+b1 = Button(w, text="박스 ", bg = "red", fg = "white")
+b1.place(x=0, y=0)
+b2 = Button(w, text="박스 #2", bg = "green", fg = "black")
+b2.place(x=20, y=30)
+b3 = Button(w, text="박스 #3", bg = "orange", fg = "white")
+b3.place(x=40, y=60)
+
+w.mainloop()
